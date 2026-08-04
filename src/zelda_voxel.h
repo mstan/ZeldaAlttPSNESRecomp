a560e62b5b7b7b23784478e5f3a7207b512d7e86
Matthew Stanley <mstanley@particle.io>
2026-08-03 17:31:19 -0700

    feat(voxel): add ALTTP orbit experiment

---
+++ src/zelda_voxel.h
@@ -0,0 +1,13 @@
+#ifndef ZELDA_VOXEL_H
+#define ZELDA_VOXEL_H
+
+#include <stddef.h>
+#include <stdint.h>
+#include <SDL.h>
+
+void ZeldaVoxelHandleEvent(const SDL_Event *event);
+uint32_t ZeldaVoxelRemapInput(uint32_t input);
+void ZeldaVoxelPostRender(uint8_t *pixels, size_t pitch,
+                          int width, int height);
+
+#endif
